c5b9483a343769758344e6d694dcc63297efae84
Julien Leicher <jleicher@gmail.com>
2015-02-11 16:26:56 +0100

    Edited package informations

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'yuukan:streamy',
-  version: '0.9.0',
+  version: '0.9.1',
   // Brief, one-line summary of the package.
   summary: 'Simple interface to use the underlying sockjs in a meteor application',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/YuukanOO/streamy',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
